f4565f6860eadf0b28f0535009f463ae213bfe62
Jiani Wang <wjn9424@163.com>
2022-04-30 20:19:21 -0700

    Initial Commit

---
+++ contact_tracing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  contact_tracing
+//
+//  Created by Jiani Wang on 2022/4/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
