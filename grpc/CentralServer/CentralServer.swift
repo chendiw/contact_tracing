cf181ae5fc5153c6bc880a7a9abd5e8e6e7c4381
Angela Montemayor <angelamontemayor@me.com>
2022-06-01 22:59:48 -0700

    basic central server runs

--- grpc/CentralServer/CentralServer.swift
+++ grpc/CentralServer/CentralServer.swift
@@ -1,9 +1,10 @@
 import GRPC
-import HelloWorldModel
+import centralModel
 import NIOCore
+import Foundation
 
 class CentralProvider: Central_CentralProvider {
-  var interceptors: Central_CentralClientInterceptorFactoryProtocol?
+  var interceptors: Central_CentralServerInterceptorFactoryProtocol?
 
   func sendExposureKeys(
     request: Central_ExposureKeys,
@@ -11,56 +12,78 @@ class CentralProvider: Central_CentralProvider {
   ) -> EventLoopFuture<Central_Ack> {
     // write tokens to file, with delimiters
     if (request.pos == 1) {
-        FileManager.default.createDirectory(atPath: "pos-" + request.date1, withIntermediateDirectories: true, attributes: nil)
+        do {
+          try FileManager.default.createDirectory(atPath: "pos-" + request.date1.date, withIntermediateDirectories: true, attributes: nil)
+        } catch {
+          print(error)
+        }
         let token1 = request.token1
-        var token1Str = String(token1) + "$"
-        let path = FileManager.default.urls(for: .documentDirectory,
-                                    in: .userDomainMask)[0].appendingPathComponent("pos-" + request.date1)
+        let token1Str = String(token1) + "$"
+        var path = FileManager.default.urls(for: .documentDirectory,
+                                    in: .userDomainMask)[0].appendingPathComponent("pos-" + request.date1.date)
         if let stringData = token1Str.data(using: .utf8) {
                 try? stringData.write(to: path)
         }
 
-        FileManager.default.createDirectory(atPath: "pos-" + request.date2, withIntermediateDirectories: true, attributes: nil)
+        do {
+          try FileManager.default.createDirectory(atPath: "pos-" + request.date2.date, withIntermediateDirectories: true, attributes: nil)
+        } catch {
+          print(error)
+        }
         let token2 = request.token2
-        var token2Str = String(token2) + "$"
-        let path = FileManager.default.urls(for: .documentDirectory,
-                                    in: .userDomainMask)[0].appendingPathComponent("pos-" + request.date2)
+        let token2Str = String(token2) + "$"
+        path = FileManager.default.urls(for: .documentDirectory,
+                                    in: .userDomainMask)[0].appendingPathComponent("pos-" + request.date2.date)
         if let stringData = token2Str.data(using: .utf8) {
                 try? stringData.write(to: path)
         }
 
-        FileManager.default.createDirectory(atPath: "pos-" + request.date3, withIntermediateDirectories: true, attributes: nil)
+        do {
+          try FileManager.default.createDirectory(atPath: "pos-" + request.date3.date, withIntermediateDirectories: true, attributes: nil)
+        } catch {
+          print(error)
+        }
         let token3 = request.token3
-        var token3Str = String(token3) + "$"
-        let path = FileManager.default.urls(for: .documentDirectory,
-                                    in: .userDomainMask)[0].appendingPathComponent("pos-" + request.date3)
+        let token3Str = String(token3) + "$"
+        path = FileManager.default.urls(for: .documentDirectory,
+                                    in: .userDomainMask)[0].appendingPathComponent("pos-" + request.date3.date)
         if let stringData = token3Str.data(using: .utf8) {
                 try? stringData.write(to: path)
         }
 
-        FileManager.default.createDirectory(atPath: "pos-" + request.date4, withIntermediateDirectories: true, attributes: nil)
+        do {
+          try FileManager.default.createDirectory(atPath: "pos-" + request.date4.date, withIntermediateDirectories: true, attributes: nil)
+        } catch {
+          print(error)
+        }
         let token4 = request.token4
-        var token4Str = String(token4) + "$"
-        let path = FileManager.default.urls(for: .documentDirectory,
-                                    in: .userDomainMask)[0].appendingPathComponent("pos-" + request.date4)
+        let token4Str = String(token4) + "$"
+        path = FileManager.default.urls(for: .documentDirectory,
+                                    in: .userDomainMask)[0].appendingPathComponent("pos-" + request.date4.date)
         if let stringData = token4Str.data(using: .utf8) {
                 try? stringData.write(to: path)
         }
 
-        FileManager.default.createDirectory(atPath: "pos-" + request.date5, withIntermediateDirectories: true, attributes: nil)
+        do {
+          try FileManager.default.createDirectory(atPath: "pos-" + request.date5.date, withIntermediateDirectories: true, attributes: nil)
+        } catch {
+          print(error)
+        }
         let token5 = request.token5
-        var token5Str = String(token5) + "$"
-        let path = FileManager.default.urls(for: .documentDirectory,
-                                    in: .userDomainMask)[0].appendingPathComponent("pos-" + request.date5)
+        let token5Str = String(token5) + "$"
+        path = FileManager.default.urls(for: .documentDirectory,
+                                    in: .userDomainMask)[0].appendingPathComponent("pos-" + request.date5.date)
         if let stringData = token5Str.data(using: .utf8) {
                 try? stringData.write(to: path)
         }
     } else {
-        FileManager.default.createDirectory(atPath: "neg-" + request.date1, withIntermediateDirectories: true, attributes: nil)
-        FileManager.default.createDirectory(atPath: "neg-" + request.date1, withIntermediateDirectories: true, attributes: nil)
-        FileManager.default.createDirectory(atPath: "neg-" + request.date1, withIntermediateDirectories: true, attributes: nil)
-        FileManager.default.createDirectory(atPath: "neg-" + request.date1, withIntermediateDirectories: true, attributes: nil)
-        FileManager.default.createDirectory(atPath: "neg-" + request.date1, withIntermediateDirectories: true, attributes: nil)
+        /*
+        FileManager.default.createDirectory(atPath: "neg-" + request.date1.date, withIntermediateDirectories: true, attributes: nil)
+        FileManager.default.createDirectory(atPath: "neg-" + request.date2.date, withIntermediateDirectories: true, attributes: nil)
+        FileManager.default.createDirectory(atPath: "neg-" + request.date3.date, withIntermediateDirectories: true, attributes: nil)
+        FileManager.default.createDirectory(atPath: "neg-" + request.date4.date, withIntermediateDirectories: true, attributes: nil)
+        FileManager.default.createDirectory(atPath: "neg-" + request.date5.date, withIntermediateDirectories: true, attributes: nil)
+        */
     }
     let response = Central_Ack.with {
       $0.ack = 1
@@ -74,7 +97,8 @@ class CentralProvider: Central_CentralProvider {
   ) -> EventLoopFuture<Central_Batch> {
     // need to pull tokens from appropriate file
     let response = Central_Batch.with {
-      
+      let x = UInt64(123)
+      $0.token = [x];
     }
     return context.eventLoop.makeSucceededFuture(response)
   }
@@ -85,7 +109,8 @@ class CentralProvider: Central_CentralProvider {
   ) -> EventLoopFuture<Central_Batch> {
     // need to pull tokens from appropriate file
     let response = Central_Batch.with {
-      
+      let x = UInt64(123)
+      $0.token = [x];
     }
     return context.eventLoop.makeSucceededFuture(response)
   }
